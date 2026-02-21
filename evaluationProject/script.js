009fb7a3c3c68747ac5ffcae487a7fc78e9f3401
Wenli Du <wenlidu2023@gmail.com>
2026-02-21 07:09:15 +0000

    add snake part and update code

--- evaluationProject/script.js
+++ evaluationProject/script.js
@@ -1,12 +1,15 @@
-const START_TIME = 30;
+const START_TIME = 20;
 const START_SCORES = 0;
 
 class WhacAMoleModel {
     scores;
     timeLeft;
-    constructor() {
+    molesShown;
+    constructor(view) {
+        this.view = view;
         this.scores = START_SCORES;
         this.timeLeft = START_TIME;
+        this.molesShown = [];
     }
     increment(num) {
         this.scores += num;
@@ -43,45 +46,59 @@ class WhacAMoleController {
     }
     
     bindMoleEvent(model, view) {
+        view.render(model.scores, model.timeLeft);
         let curPos = 1;
-        let molesShown = [];
+        let snakePos = 1;
+        let snakeElem = document.querySelector('#mole' + snakePos);
+        let moleElem = document.querySelector('#mole' + curPos);
+        view.bindListener(view.buttonStart, ()=> {
+            model.reset(); 
+            let timer = setInterval(() => {
+                if(model.molesShown.length == 3) {
+                    this.setBlank();
+                }
+                curPos = Math.floor(Math.random() * 12) + 1;
+                moleElem = document.querySelector('#mole' + curPos);
+                moleElem.style.backgroundImage = "url('./images/mole.jpg')";
+                model.molesShown.push(moleElem);  
+                if(model.timeLeft % 2 == 0) {
+                    snakeElem.style.backgroundImage =  'none';
+                    snakePos = Math.floor(Math.random() * 12 + 1); 
+                    snakeElem = document.querySelector('#mole' + snakePos);              
+                    snakeElem.style.backgroundImage = "url('./images/snake.jpg')";
+                }                 
+                model.timeLeft--; 
+                view.render(model.scores, model.timeLeft);                                
+                if(model.timeLeft <= 0) {
+                    model.timeLeft = 0;
+                    this.setBlank();
+                    model.molesShown.length = 0;
+                    moleElem.style.backgroundImage =  'none';
+                    snakeElem.style.backgroundImage =  'none';
+                    view.render(model.scores, model.timeLeft);
+                    clearInterval(timer);
+                }                 
+            }, 1000);
+        });
         view.moles.forEach((mole) => {
             view.bindListener(mole, ()=> {
-                if(molesShown.includes(mole)) {
+                if(mole == snakeElem) {
+                    snakeElem.style.backgroundImage =  'none';
+                    this.setBlank();
+                    model.timeLeft = 0;
+                    for(let elem of model.molesShown) elem.style.backgroundImage = 'none';
+                } else if(model.molesShown.includes(mole)) {
                     model.increment(1);
                 }
             });
         });
-        
-        view.bindListener(view.buttonStart, ()=> {
-            model.reset(); 
-            let elem = null;
-            let timer = setInterval(() => {
-                model.timeLeft--;                
-                let showTimer = setInterval(()=> {
-                    if(molesShown.length == 3) {
-                        while(molesShown.length > 0){
-                            elem = molesShown.shift();
-                            elem.style.backgroundImage = 'none';
-                        }
-                    }
-                    curPos = Math.floor(Math.random() * 12 + 1);
-                    elem = document.querySelector('#mole' + curPos)
-                    elem.style.backgroundImage = "url('./images/mole.jpg')";
-                    molesShown.push(elem);
-                    if(model.timeLeft <= 0) {
-                        document.querySelector('#mole' + curPos).style.backgroundImage = 'none';
-                        clearInterval(showTimer);
-                    }
-                }, 1000);
-                view.render(model.scores, model.timeLeft);
-                if(model.timeLeft <= 0) {
-                    clearInterval(timer);
-                }
-            }, 1000);
-        });
     }
-
+    setBlank(){
+        while(this.model.molesShown.length > 0) {
+            let moleElem = model.molesShown.shift();
+            moleElem.style.backgroundImage = 'none';
+        }
+    }
 }
 
 const view = new WhacAMoleView();
